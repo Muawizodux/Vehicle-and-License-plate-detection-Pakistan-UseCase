46c106bdb26d5e01fd206d985c2be8b79ad64548
Muawiz Umar <88239592+Muawizodux@users.noreply.github.com>
2024-05-01 15:03:54 +0000

    add utils

---
+++ utils.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+
+def determine_score(arr, angle):
+        data = inter.rotate(arr, angle, reshape=False, order=0)
+        histogram = np.sum(data, axis=1, dtype=float)
+        score = np.sum((histogram[1:] - histogram[:-1]) ** 2, dtype=float)
+        return histogram, score
+
+def correct_skew(image, delta=20, limit=14):
+    thresh =image
+    scores = []
+    angles = np.arange(-limit, limit + delta, delta)
+    for angle in angles:
+        histogram, score = determine_score(thresh, angle)
+        scores.append(score)
+    best_angle = angles[scores.index(max(scores))]
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, best_angle, 1.0)
+    corrected = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, \
+            borderMode=cv2.BORDER_REPLICATE)
+    return best_angle, corrected
