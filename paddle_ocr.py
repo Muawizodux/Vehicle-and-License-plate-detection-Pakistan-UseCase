0f5751aa95befe39d3a8364f5f0f0ad17dde8b54
Muawiz Umar <88239592+Muawizodux@users.noreply.github.com>
2024-05-01 17:58:50 +0000

    test

--- paddle_ocr.py
+++ paddle_ocr.py
@@ -3,12 +3,12 @@ import numpy as np
 from paddleocr import PaddleOCR
 from utils import preprocess_lp
 
-image = cv2.imread('')
-cropped_region = preprocess_lp(image)
-image_np = np.array(cropped_region)
+#image = cv2.imread('/workspaces/Vehicle-and-License-plate-detection-Pakistan-UseCase/assets/')
+# cropped_region = preprocess_lp(image)
+# image_np = np.array(cropped_region)
 
 ocr = PaddleOCR()
-result = ocr.ocr(image_np)
+# result = ocr.ocr(image_np)
 
-license_plate_text = '\n'.join([' '.join(str(item) for item in line) for _, line in result[0]])
-print("License plate Number :", license_plate_text.split()[0])
+# license_plate_text = '\n'.join([' '.join(str(item) for item in line) for _, line in result[0]])
+# print("License plate Number :", license_plate_text.split()[0])
